38152ff5aa2e35ec249e0c1f3483162b8fd9fa0d
Sonlis <bastien.jeannelle@gmail.com>
2024-06-11 10:40:24 +0300

    Add Lua language server

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -5,31 +5,32 @@ require("config.lsp-commands")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  vim.fn.system({
-    "git",
-    "clone",
-    "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", -- latest stable release
-    lazypath,
-  })
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", -- latest stable release
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
-  'camspiers/snap',
-  {'rose-pine/neovim',
-    as = 'rose-pine',
+    'camspiers/snap',
+    {
+        'rose-pine/neovim',
+        as = 'rose-pine',
         config = function()
             require("config.plugins.rosepine_config")
         end
-  },
-  'nvim-treesitter/nvim-treesitter',
-  'mbbill/undotree',
-  'tpope/vim-fugitive',
-  {
-    'nvim-neo-tree/neo-tree.nvim',
-    dependencies = {
+    },
+    'nvim-treesitter/nvim-treesitter',
+    'mbbill/undotree',
+    'tpope/vim-fugitive',
+    {
+        'nvim-neo-tree/neo-tree.nvim',
+        dependencies = {
             "nvim-lua/plenary.nvim",
             "nvim-tree/nvim-web-devicons",
             "MunifTanjim/nui.nvim"
@@ -38,9 +39,9 @@ require("lazy").setup({
             require("config.plugins.neotree_config")
         end
     },
-  {
-      'altermo/ultimate-autopair.nvim',
-      event = {'InsertEnter','CmdlineEnter'},
-      branch = "v0.6"
-  }
+    {
+        'altermo/ultimate-autopair.nvim',
+        event = { 'InsertEnter', 'CmdlineEnter' },
+        branch = "v0.6"
+    }
 })
