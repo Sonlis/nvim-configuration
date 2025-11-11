bcd901195b359fefbdc99729a7a83efb01b1ecbd
bastien.jeannelle <bastien.jeannelle@infomaniak.com>
2025-11-11 15:33:05 +0100

    feat(swayidle): put computer to sleep after period of time

--- modules/home-manager/foreground/desktop/wayland/niri/swayidle.nix
+++ modules/home-manager/foreground/desktop/wayland/niri/swayidle.nix
@@ -15,7 +15,7 @@
       };
       Service = {
         Restart = "on-failure";
-        ExecStart = "${pkgs.swayidle}/bin/swayidle -w timeout 601 'niri msg action power-off-monitors' timeout 600 '${pkgs.swaylock-effects}/bin/swaylock -f' before-sleep '${pkgs.swaylock-effects}/bin/swaylock --clock --indicator -f'";
+        ExecStart = "${pkgs.swayidle}/bin/swayidle -w timeout 450 'niri msg action power-off-monitors' timeout 300 '${pkgs.swaylock-effects}/bin/swaylock --clock --indicator -f' timeout 600 'systemctl suspend' before-sleep '${pkgs.swaylock-effects}/bin/swaylock --clock --indicator -f'";
       };
     };
   };
