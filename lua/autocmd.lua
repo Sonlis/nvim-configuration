717bf5eb899ec5e81b14658a8057754240ca9482
bastien.jeannelle <bastien.jeannelle@infomaniak.com>
2025-11-18 21:03:28 +0100

    autocmds, use ruff ty as py lsp

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -10,7 +10,7 @@ local on_save = augroup('OnSave', {})
 -- Neat little trick to open neotree on startup:
 -- If we open giving a directory or no arguments, opens neotree in a way that it remains on the left
 -- If a file is given as argument, do not open neotree
-autocmd({"VimEnter"}, {
+autocmd({ "VimEnter" }, {
     group = editor_startup,
     callback = function()
         local entrypoint = vim.fn.expand('<afile>')
@@ -78,16 +78,16 @@ vim.api.nvim_create_autocmd('LspAttach', {
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-  callback = function(ev)
-    local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    if client:supports_method('textDocument/completion') then
-      vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
-    end
-  end,
+    callback = function(ev)
+        local client = vim.lsp.get_client_by_id(ev.data.client_id)
+        if client:supports_method('textDocument/completion') then
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+        end
+    end,
 })
 
 -- Magic to make neovim theme fit the entire foot window
-vim.api.nvim_create_autocmd({'UIEnter', 'ColorScheme'}, {
+vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
     callback = function()
         local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
         if normal.bg then
@@ -107,5 +107,61 @@ vim.api.nvim_create_autocmd("BufWritePre", {
     buffer = bufnr,
     callback = function()
         vim.lsp.buf.format()
+    end,
+})
+
+-- auto resize splits when the terminal's window is resized
+vim.api.nvim_create_autocmd("VimResized", {
+    command = "wincmd =",
+})
+
+-- no auto continue comments on new line
+vim.api.nvim_create_autocmd("FileType", {
+    group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+    callback = function()
+        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+    end,
+})
+
+-- syntax highlighting for dotenv files
+vim.api.nvim_create_autocmd("BufRead", {
+    group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
+    pattern = { ".env", ".env.*" },
+    callback = function()
+        vim.bo.filetype = "dosini"
+    end,
+})
+
+-- ide like highlight when stopping cursor
+vim.api.nvim_create_autocmd("CursorMoved", {
+    group = vim.api.nvim_create_augroup("LspReferenceHighlight", { clear = true }),
+    desc = "Highlight references under cursor",
+    callback = function()
+        -- Only run if the cursor is not in insert mode
+        if vim.fn.mode() ~= "i" then
+            local clients = vim.lsp.get_clients({ bufnr = 0 })
+            local supports_highlight = false
+            for _, client in ipairs(clients) do
+                if client.server_capabilities.documentHighlightProvider then
+                    supports_highlight = true
+                    break -- Found a supporting client, no need to check others
+                end
+            end
+
+            -- 3. Proceed only if an LSP is active AND supports the feature
+            if supports_highlight then
+                vim.lsp.buf.clear_references()
+                vim.lsp.buf.document_highlight()
+            end
+        end
+    end,
+})
+
+-- ide like highlight when stopping cursor
+vim.api.nvim_create_autocmd("CursorMovedI", {
+    group = "LspReferenceHighlight",
+    desc = "Clear highlights when entering insert mode",
+    callback = function()
+        vim.lsp.buf.clear_references()
     end,
 })
