0a2360a5aa8e5849e0a457d3c4c1fa8a94bd2ce3
bastien.jeannelle <bastien.jeannelle@infomaniak.com>
2025-12-30 19:29:39 +0100

    feat(niri): Remove flake, depend only on nixpkgs

--- flake.nix
+++ flake.nix
@@ -30,11 +30,6 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
-    niri = {
-      url = "github:sodiboo/niri-flake";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
-
     dankMaterialShell = {
       url = "github:AvengeMedia/DankMaterialShell";
       inputs.nixpkgs.follows = "nixpkgs";
