6177b865c78267dbf198859bd24c32bf743c6de4
bastien.jeannelle <bastien.jeannelle@infomaniak.com>
2026-02-06 11:40:02 +0100

    tmux bindings, fix ssh options

--- modules/home-manager/background/programs/git.nix
+++ modules/home-manager/background/programs/git.nix
@@ -18,20 +18,22 @@
 
   config = {
     programs.git = {
+      enable = true;
+      signing = {
+        key = config.git.signingKey;
+        signByDefault = true;
+      };
+      extraConfig = {
+        safe.directory = [ "/etc/nixos" ];
+        pull.rebase = true;
+        gpg = {
+          format = "ssh";
+        };
+      };
       settings = {
         user = {
           name = "${config.git.userName}";
           email = "${config.git.email}";
-          safe.directory = [ "/etc/nixos" ];
-          pull.rebase = true;
-          gpg = {
-            format = "ssh";
-          };
-        };
-        enable = true;
-        signing = {
-          key = config.git.signingKey;
-          signByDefault = true;
         };
       };
     };
--- modules/home-manager/foreground/terminal/tmux.nix
+++ modules/home-manager/foreground/terminal/tmux.nix
@@ -2,6 +2,8 @@
 {
   programs.tmux = {
     enable = true;
+    keyMode = "vi";
+    shortcut = "a";
     extraConfig = "bind -T copy-mode-vi y send -X copy-selection";
     plugins = [ pkgs.tmuxPlugins.catppuccin ];
   };
