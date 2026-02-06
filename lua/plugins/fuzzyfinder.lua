ff3d489ea3609aa14771dd7e18d5367e20bbfecf
bastien.jeannelle <bastien.jeannelle@infomaniak.com>
2026-02-06 17:32:48 +0100

    use telescope instead of snaps

--- lua/plugins/fuzzyfinder.lua
+++ lua/plugins/fuzzyfinder.lua
@@ -1,11 +1,17 @@
 return {
-    'camspiers/snap',
+    'nvim-telescope/telescope.nvim',
+    version = '*',
+    dependencies = {
+        'nvim-lua/plenary.nvim',
+        -- optional but recommended
+        { 'nvim-telescope/telescope-fzf-native.nvim', build = 'make' },
+    },
     config = function()
-        local snap = require("snap")
-        snap.maps {
-            { "<Leader><Leader>", snap.config.file { producer = "ripgrep.file" } },
-            { "<leader>g",        snap.config.vimgrep({}) },
-            { "<Leader>B",        snap.config.file {producer = "vim.buffer"} },
-        }
+        local builtin = require('telescope.builtin')
+        vim.keymap.set('n', '<leader> ', builtin.find_files, { desc = 'Telescope find files' })
+        vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = 'Telescope live grep' })
+        vim.keymap.set('n', '<leader>fb', builtin.buffers, { desc = 'Telescope buffers' })
+        vim.keymap.set('n', '<leader>fh', builtin.help_tags, { desc = 'Telescope help tags' })
     end
+
 }
