3363f7fcedf7d4e0d4f75a615c2e1f8efef9a69b
Sonlis <bastien.jeannelle@gmail.com>
2026-08-06 11:34:18 +0200

    chore: add some colorschemes

--- plugin/colorscheme.lua
+++ plugin/colorscheme.lua
@@ -1,16 +1,23 @@
 vim.pack.add({ { src = "https://github.com/catppuccin/nvim", name = "catppuccin" } })
 
 require('catppuccin').setup({
-    transparent_background = false
+  transparent_background = false
 })
 
 vim.pack.add({ { src = "https://github.com/rose-pine/neovim", name = "rose-pine" } })
 require("rose-pine").setup({
-    styles = {
-        bold = true,
-        italic = true,
-        transparency = true,
-    },
+  styles = {
+    bold = true,
+    italic = true,
+    transparency = true,
+  },
 })
 
-vim.cmd.colorscheme "rose-pine"
+
+vim.pack.add({ { src = "https://github.com/gbprod/nord.nvim", name = "nord" } })
+vim.pack.add({ { src = "https://github.com/folke/tokyonight.nvim", name = "tokyonight" } })
+vim.pack.add { "https://github.com/neanias/everforest-nvim" }
+require("everforest").setup({
+  transparent_background_level = 1
+})
+vim.cmd.colorscheme "rose-pine-main"
