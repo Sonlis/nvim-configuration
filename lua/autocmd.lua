0a383547eb8db531d6915d054aba3a12b6106779
Sonlis <bastien.jeannelle@gmail.com>
2026-08-11 09:57:52 +0200

    chore: remove unused plugin

--- lua/autocmd.lua
+++ lua/autocmd.lua
@@ -6,146 +6,143 @@ local yank_group = augroup('HighlightYank', {})
 local on_save = augroup('OnSave', {})
 
 autocmd('TextYankPost', {
-    group = yank_group,
-    pattern = '*',
-    callback = function()
-        vim.highlight.on_yank({
-            higroup = 'IncSearch',
-            timeout = 40,
-        })
-    end,
+  group = yank_group,
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = 'IncSearch',
+      timeout = 40,
+    })
+  end,
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    desc = 'LSP actions',
-    callback = function(event)
-        local bufmap = function(mode, lhs, rhs)
-            local opts = { buffer = event.buf }
-            vim.keymap.set(mode, lhs, rhs, opts)
-        end
-
-        -- You can find details of these function in the help page
-        -- see for example, :help vim.lsp.buf.hover()
-
-        -- Trigger code completion
-        bufmap('i', '<C-Space>', '<C-x><C-o>')
-
-        -- Display documentation of the symbol under the cursor
-        bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
-
-        -- Jump to the definition
-        bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
-
-        -- Jump to declaration
-        bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
-
-        -- Lists all the implementations for the symbol under the cursor
-        bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
-
-        -- Jumps to the definition of the type symbol
-        bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
-
-        -- Lists all the references
-        bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
-
-        -- Displays a function's signature information
-        bufmap('n', 'gk', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
-
-        -- Renames all references to the symbol under the cursor
-        bufmap('n', '<gR>', '<cmd>lua vim.lsp.buf.rename()<cr>')
-
-        -- Format current file
-        bufmap('n', '<gF>', '<cmd>lua vim.lsp.buf.format()<cr>')
-
-        -- Selects a code action available at the current cursor position
-        bufmap('n', '<C-å>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+  desc = 'LSP actions',
+  callback = function(event)
+    local bufmap = function(mode, lhs, rhs)
+      local opts = { buffer = event.buf }
+      vim.keymap.set(mode, lhs, rhs, opts)
     end
+
+    -- You can find details of these function in the help page
+    -- see for example, :help vim.lsp.buf.hover()
+
+    -- Trigger code completion
+    bufmap('i', '<C-Space>', '<C-x><C-o>')
+
+    -- Display documentation of the symbol under the cursor
+    bufmap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>')
+
+    -- Jump to the definition
+    bufmap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>')
+
+    -- Jump to declaration
+    bufmap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>')
+
+    -- Lists all the implementations for the symbol under the cursor
+    bufmap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>')
+
+    -- Jumps to the definition of the type symbol
+    bufmap('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>')
+
+    -- Lists all the references
+    bufmap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>')
+
+    -- Displays a function's signature information
+    bufmap('n', 'gk', '<cmd>lua vim.lsp.buf.signature_help()<cr>')
+
+    -- Format current file
+    bufmap('n', '<gF>', '<cmd>lua vim.lsp.buf.format()<cr>')
+
+    -- Selects a code action available at the current cursor position
+    bufmap('n', '<C-å>', '<cmd>lua vim.lsp.buf.code_action()<cr>')
+  end
 })
 
 vim.api.nvim_create_autocmd('LspAttach', {
-    callback = function(ev)
-        local client = vim.lsp.get_client_by_id(ev.data.client_id)
-    end,
+  callback = function(ev)
+    local client = vim.lsp.get_client_by_id(ev.data.client_id)
+  end,
 })
 
 -- Magic to make neovim theme fit the entire foot window
 vim.api.nvim_create_autocmd({ 'UIEnter', 'ColorScheme' }, {
-    callback = function()
-        local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
-        if normal.bg then
-            io.write(string.format('\027]11;#%06x\027\\', normal.bg))
-        end
-    end,
+  callback = function()
+    local normal = vim.api.nvim_get_hl(0, { name = 'Normal' })
+    if normal.bg then
+      io.write(string.format('\027]11;#%06x\027\\', normal.bg))
+    end
+  end,
 })
 
 vim.api.nvim_create_autocmd('UILeave', {
-    callback = function()
-        io.write('\027]111\027\\')
-    end,
+  callback = function()
+    io.write('\027]111\027\\')
+  end,
 })
 
 -- Format file on save
 vim.api.nvim_create_autocmd("BufWritePre", {
-    group = on_save,
-    buffer = bufnr,
-    callback = function()
-        vim.lsp.buf.format()
-    end,
+  group = on_save,
+  buffer = bufnr,
+  callback = function()
+    vim.lsp.buf.format()
+  end,
 })
 
 -- auto resize splits when the terminal's window is resized
 vim.api.nvim_create_autocmd("VimResized", {
-    command = "wincmd =",
+  command = "wincmd =",
 })
 
 -- no auto continue comments on new line
 vim.api.nvim_create_autocmd("FileType", {
-    group = vim.api.nvim_create_augroup("no_auto_comment", {}),
-    callback = function()
-        vim.opt_local.formatoptions:remove({ "c", "r", "o" })
-    end,
+  group = vim.api.nvim_create_augroup("no_auto_comment", {}),
+  callback = function()
+    vim.opt_local.formatoptions:remove({ "c", "r", "o" })
+  end,
 })
 
 
 -- syntax highlighting for dotenv files
 vim.api.nvim_create_autocmd("BufRead", {
-    group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
-    pattern = { ".env", ".env.*" },
-    callback = function()
-        vim.bo.filetype = "dosini"
-    end,
+  group = vim.api.nvim_create_augroup("dotenv_ft", { clear = true }),
+  pattern = { ".env", ".env.*" },
+  callback = function()
+    vim.bo.filetype = "dosini"
+  end,
 })
 
 -- ide like highlight when stopping cursor
 vim.api.nvim_create_autocmd("CursorMoved", {
-    group = vim.api.nvim_create_augroup("LspReferenceHighlight", { clear = true }),
-    desc = "Highlight references under cursor",
-    callback = function()
-        -- Only run if the cursor is not in insert mode
-        if vim.fn.mode() ~= "i" then
-            local clients = vim.lsp.get_clients({ bufnr = 0 })
-            local supports_highlight = false
-            for _, client in ipairs(clients) do
-                if client.server_capabilities.documentHighlightProvider then
-                    supports_highlight = true
-                    break -- Found a supporting client, no need to check others
-                end
-            end
-
-            -- 3. Proceed only if an LSP is active AND supports the feature
-            if supports_highlight then
-                vim.lsp.buf.clear_references()
-                vim.lsp.buf.document_highlight()
-            end
+  group = vim.api.nvim_create_augroup("LspReferenceHighlight", { clear = true }),
+  desc = "Highlight references under cursor",
+  callback = function()
+    -- Only run if the cursor is not in insert mode
+    if vim.fn.mode() ~= "i" then
+      local clients = vim.lsp.get_clients({ bufnr = 0 })
+      local supports_highlight = false
+      for _, client in ipairs(clients) do
+        if client.server_capabilities.documentHighlightProvider then
+          supports_highlight = true
+          break           -- Found a supporting client, no need to check others
         end
-    end,
+      end
+
+      -- 3. Proceed only if an LSP is active AND supports the feature
+      if supports_highlight then
+        vim.lsp.buf.clear_references()
+        vim.lsp.buf.document_highlight()
+      end
+    end
+  end,
 })
 
 -- ide like highlight when stopping cursor
 vim.api.nvim_create_autocmd("CursorMovedI", {
-    group = "LspReferenceHighlight",
-    desc = "Clear highlights when entering insert mode",
-    callback = function()
-        vim.lsp.buf.clear_references()
-    end,
+  group = "LspReferenceHighlight",
+  desc = "Clear highlights when entering insert mode",
+  callback = function()
+    vim.lsp.buf.clear_references()
+  end,
 })
